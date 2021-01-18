9912e84dd4a66821cc2ebd1f6a43a9bde6618aae
Telicia Watson <teli203@aol.com>
2021-01-18 12:20:49 -0600

    add src and index.js

---
+++ public/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="navbar-brand mx-auto">Employee Directory</div>
+        </nav>
+    );
+}
+
+export default Navbar;
